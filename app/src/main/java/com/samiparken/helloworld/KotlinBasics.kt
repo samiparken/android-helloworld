ae16670c7b1b54d699ecda58344e3b3e1e822bef
Han-Saem Park <samiparken@gmail.com>
2021-06-08 22:53:56 +0200

    Kotlin Basics 2

--- app/src/main/java/com/samiparken/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/samiparken/helloworld/KotlinBasics.kt
@@ -32,6 +32,7 @@ fun main() {
     val lastChar = myStr[myStr.length - 1]
 
     /* Arithmetic operators (+, -, *, /, %) */
+    /* Assignment operators (+, -, *, /, %) */
     var result = 5+3
     result /= 2
     result *= 2
@@ -50,5 +51,88 @@ fun main() {
     println("Is 5 greater 3 ? ${5>3}")
     println("Is 5 Lower Equal 3 ? ${5<=3}")
 
+    /* Increment & Decrement operators (++, --)*/
+    println("result is $result")
+    println("result is ${result++}")
+    println("result is ${++result}")
+
+    /* if statement */
+    var isActive = true
+    if (isActive) {
+        println("true")
+    } else {
+        println("false")
+    }
+
+    /* when expressions */
+    var season = 3
+    when(season) {
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> print("Invalid Season")
+    }
+
+    var month = 3
+    when(month) {
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        else -> print("Invalid Month")
+    }
+
+    var x : Any = 13.37f
+    when(x) {
+        is Int -> println("$x is an Int")
+        is Double -> println("$x is a Double")
+        is String -> print("$x is a String")
+        !is String -> print("$x is not a String")
+        else -> println("$x is none of the above")
+    }
+    print("\n")
+
+    /* while loop */
+    var w = 1
+    while ( w <= 10) {
+        print("$w\n")
+        w++
+    }
+
+    /* do-while loop */
+    var y = 1
+    do {
+        print("$y\n")
+        y++
+    } while (y > 10)
+    println("$y\n")
+
+    var feltTemp = "cold"
+    var roomTemp = 10
+    while (feltTemp == "cold") {
+        roomTemp++
+        if (roomTemp >= 20) {
+            feltTemp = "Comfy"
+            println("It's comfy now")
+        }
+    }
+
+    /* for loops */
+    for( num in 1..10 ) {
+        print("$num")
+    }
+    for( num2 in 1 until 10 ) { // same as 1.until(10)
+        print("$num2")
+    }
+    for( num3 in 10 downTo 1 ) {
+        print("$num3")
+    }
+    for( num4 in 10 downTo 1 step 2 ) {   // same as 10.downTo(1).step(2)
+        print("$num4")
+    }
 
 }
